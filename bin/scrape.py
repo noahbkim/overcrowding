94b9c32a1857b0e5250661d8b5dade6bea9f23a3
Noah Kim <noahbkim@gmail.com>
2018-03-05 11:03:06 -0500

    Found some open data

--- bin/scrape.py
+++ bin/scrape.py
@@ -1,11 +1,27 @@
 import requests
 import re
+import os
 
 
 SCHOOL_LIST = "http://www.montgomeryschoolsmd.org/schools/#school_list"
 SCHOOL_REPORT = "http://www.montgomeryschoolsmd.org/departments/regulatoryaccountability/glance/currentyear/schools/"
 
 SCHOOL_ID_PATTERN = r"schooloverview\.aspx\?s\=(\d+)"
+
+ROOT_DIRECTORY = os.path.dirname(os.path.dirname(__file__))
+REPORT_DIRECTORY = os.path.join(ROOT_DIRECTORY, "data", "reports")
+
+
+def with_directories(*directories):
+    """Wrapper to make sure file system directories are in place."""
+
+    def wrapper(func):
+        def wrapped(*args, **kwargs):
+            for directory in directories:
+                os.makedirs(directory, exist_ok=True)
+            func(*args, **kwargs)
+        return wrapped
+    return wrapper
 
 
 def scrape_school_ids(url: str=SCHOOL_LIST):
@@ -15,7 +31,15 @@ def scrape_school_ids(url: str=SCHOOL_LIST):
     return re.findall(SCHOOL_ID_PATTERN, page.content.decode())
 
 
-def scrape_school_report(id: str):
+@with_directories(REPORT_DIRECTORY)
+def download_school_reports():
     """Scrape a school report."""
 
-    
+    for school_id in scrape_school_ids():
+        print("Downloading " + school_id)
+        with open(os.path.join(REPORT_DIRECTORY, school_id + ".pdf"), "wb") as file:
+            data = requests.get(SCHOOL_REPORT + school_id + ".pdf").content
+            file.write(data)
+
+
+download_school_reports()
