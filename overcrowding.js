cab85ba872caf8aaa08d1031555d0e6e5ed7b53a
Noah Kim <noahbkim@gmail.com>
2018-03-07 10:04:24 -0500

    Big cleanup

--- overcrowding.js
+++ overcrowding.js
@@ -1,141 +1,132 @@
-const width = window.innerWidth;
-const height = window.innerHeight;
+const W = window.innerWidth - 300;
+const H = window.innerHeight;
 
 
-/* Create the projection of the US map. */
-let projection;
-let path;
+class Renderer {
 
-/* Create a new SVG. */
-let svg = d3.select("#overcrowding");
-svg.append("rect")
-  .attr("class", "background")
-  .attr("width", width)
-  .attr("height", height);
-
-let g = svg.append("g");
-
-/* Queue assets. */
-queue()
-  //.defer(d3.json, "/data/maryland.geojson")
-  .defer(d3.json, "/data/clusters.topojson")
-  .defer(d3.json, "/data/schools.geojson")
-  .await(draw);
-
-/* Centered clusters. */
-let cluster;
-
-
-function draw(error, clusters, schools) {
-
-  let geojson = topojson.feature(clusters, clusters.objects.clusters);
-
-  projection = d3.geo.albers().scale(1).translate([0, 0]);
-  path = d3.geo.path().projection(projection);
-  let bounds = path.bounds(geojson);
-  let scale = .95 / Math.max((bounds[1][0] - bounds[0][0]) / width, (bounds[1][1] - bounds[0][1]) / height);
-  let offset = [(width - scale * (bounds[1][0] + bounds[0][0])) / 2, (height - scale * (bounds[1][1] + bounds[0][1])) / 2];
-  projection.scale(scale).translate(offset);
-
-  g.append("path")
-    .datum(topojson.merge(clusters, clusters.objects.clusters.geometries))
-    .attr("d", path)
-    .attr("class", "maryland");
-
-  g.append("path")
-    .datum(topojson.mesh(clusters, clusters.objects.clusters, (a, b) => a !== b))
-    .attr("d", path)
-    .attr("class", "border");
-
-  g.append("g").attr("class", "clusters")
-    .selectAll("path")
-    .data(geojson.features).enter().append("path")
-    .attr("d", path)
-    .attr("class", "cluster")
-    .on("click", click);
-
-}
-
-
-function click(d) {
-  let x, y, w, h, k;
-  if (d !== undefined && d.hasOwnProperty("id") && d !== cluster) {
-    // load sidebar
-  } else {
-    // clear sidebar
+  constructor(svg) {
+    this.projection = d3.geo.albers().scale(1).translate([0, 0]);
+    this.path = d3.geo.path().projection(this.projection);
+    this.svg = svg;
+    this.background();
+    this.g = svg.append("g");
   }
 
-  /* Find the center and zoom for the state. */
-  if (d && cluster !== d) {
-    let centroid = path.centroid(d);
-    let bounds = path.bounds(d);
-    x = centroid[0];
-    y = centroid[1];
-    w = bounds[1][0] - bounds[0][0];
-    h = bounds[1][1] - bounds[0][1];
-    k = 1 / (1.5 * Math.max(w / width, h / height));
-    cluster = d;
-  } else {
-    x = width / 2;
-    y = height / 2;
-    k = 1;
-    cluster = null;
+  center(bounds) {
+    let scale = .95 / Math.max((bounds[1][0] - bounds[0][0]) / W, (bounds[1][1] - bounds[0][1]) / H);
+    let offset = [(W - scale * (bounds[1][0] + bounds[0][0])) / 2, (H - scale * (bounds[1][1] + bounds[0][1])) / 2];
+    this.projection.scale(scale).translate(offset);
   }
 
-  /* Set the path as active and zoom. */
-  g.selectAll("path")
-    .classed("active", cluster && function(d) { return d === cluster; });
-  g.transition()
-    .duration(750)
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-    .style("stroke-width", 1.5 / k + "px");
+  background() {
+    this.svg.append("rect")
+      .attr("class", "background")
+      .attr("width", W)
+      .attr("height", H);
+  }
 
 }
 
 
+class Controller {
 
-/** Called when the shapefile download is ready.
-function onShapes(error, us, congress) {
-  if (error) throw error;
+  constructor() {
+    this.d = {clusters: null, cluster: null, schools: null, school: null};
+    this.o = {county: null, clusters: null, schools: null};
+    this.r = new Renderer(
+      d3.select("#overcrowding").append("svg")
+        .attr("width", W)
+        .attr("height", H));
+  }
 
-  g.append("path")
-    .datum(topojson.feature(us, us.objects.land))
-    .attr("id", "land")
-    .attr("d", path);
+  load(error, clusters, schools) {
+    this.d.clusters = clusters;
+    this.d.clusters.geo = topojson.feature(this.d.clusters, this.d.clusters.objects.clusters);
+    this.d.schools = schools;
+    if (!error) this.draw();
+    else console.log(error);
+  }
 
-  /* Draw the border between state.names.
-  let stateBorder = g.append("path");
-  stateBorder.datum(topojson.mesh(us, us.objects., function(a, b) { return a !== b; }))
-    .attr("id", "state-borders")
-    .attr("d", path);
+  draw() {
+    this.r.center(this.r.path.bounds(this.d.clusters.geo));
+    this.drawCounty();
+    this.drawClusters();
+    this.drawBorders();
+  }
 
-  /* Draw each individual state.names.
-  let states = g.append("g").attr("id", "states");
-  states.selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
-      .attr("d", path)
-      .attr("id", function(d, i) { return d.id; })
-      .on("click", clicked)
-      .call(function() {
-        let statesObject = document.getElementById("states");
-        for (let stateId of state.ids) {
-          for (let statePath of statesObject.children) {
-            if (+statePath.id === stateId) {
-              state.paths[stateId] = statePath;
-              break;
-            }
-          }
-        }
-        ready.done("shapes");
-      })
+  drawCounty() {
+    this.o.county = this.r.g.append("path");
+    this.o.county
+      .datum(topojson.merge(this.d.clusters, this.d.clusters.objects.clusters.geometries))
+      .attr("d", this.r.path)
+      .attr("class", "montgomery");
+  }
 
+  drawBorders() {
+    this.o.clusters = this.r.g.append("path");
+    this.o.clusters
+      .datum(topojson.mesh(this.d.clusters, this.d.clusters.objects.clusters, (a, b) => a !== b))
+      .attr("d", this.r.path)
+      .attr("class", "border");
+  }
 
-  /* Draw the border between districts.
-  g.append("path")
-    .datum(topojson.mesh(congress, congress.objects.districts, function(a, b) { return a !== b }))
-    .attr("id", "district-borders")
-    .attr("d", path)
+  drawClusters() {
+    this.r.g.append("g").attr("class", "clusters")
+      .selectAll("path")
+      .data(this.d.clusters.geo.features).enter().append("path")
+      .attr("d", this.r.path)
+      .attr("class", "cluster")
+      .on("click", this.selectCluster.bind(this));
+  }
+
+  drawSchools() {
+    g.append("g").attr("class", "schools")
+      .selectAll("path")
+      .data(this.d.schools.features).enter().append("path")
+      .attr("d", this.r.path)
+      .attr("class", "school");
+  }
+
+  main() {
+    queue()
+      .defer(d3.json, "/data/clusters.topojson")
+      .defer(d3.json, "/data/schools.geojson")
+      .await(this.load.bind(this));
+  }
+
+  selectCluster(cluster) {
+    let x, y, w, h, k;
+    if (cluster !== undefined && cluster.hasOwnProperty("id") && cluster !== this.cluster) {}
+    else {}
+
+    /* Find the center and zoom for the state. */
+    if (cluster && cluster !== this.cluster) {
+      let centroid = this.r.path.centroid(cluster);
+      let bounds = this.r.path.bounds(cluster);
+      x = centroid[0];
+      y = centroid[1];
+      w = bounds[1][0] - bounds[0][0];
+      h = bounds[1][1] - bounds[0][1];
+      k = 1 / (1.5 * Math.max(w / W, h / H));
+      this.cluster = cluster;
+    } else {
+      x = W / 2;
+      y = H / 2;
+      k = 1;
+      this.cluster = null;
+    }
+
+    /* Set the path as active and zoom. */
+
+    this.o.county.classed("active", this.cluster);
+    this.o.clusters.classed("background", cluster && (cluster => cluster === this.cluster));
+    this.r.g.transition().duration(750)
+      .attr("transform", "translate(" + W/2 + "," + H/2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-W", 1.5 / k + "px");
+  }
 
 }
-*/
+
+
+const controller = new Controller();
+controller.main();
